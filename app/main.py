a02fa3db1d192f7f810d5db780842872445b5488
Sergio Velásquez <svelasquezr1@upao.edu.pe>
2026-05-23 14:58:13 -0500

    feat: preparación prod

--- app/main.py
+++ app/main.py
@@ -49,6 +49,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "http://localhost:4173",
+        "https://smart-inspection-gs.netlify.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
